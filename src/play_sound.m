629e035390301ef9c78d48cd7a288164a6c826c7
hzariman <hzariman@gmail.com>
2022-03-23 23:46:19 -0700

    Revised and Added Comments

--- src/play_sound.m
+++ src/play_sound.m
@@ -1,6 +1,5 @@
+% Plays the signal at given sampling frequency
 function play_sound(wave, sample_freq )
-%PLAY_SOUND Summary of this function goes here
-%   Detailed explanation goes here
     sound(wave,sample_freq)
 end
 
